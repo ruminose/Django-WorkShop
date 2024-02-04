858f857dc86f07710c4c2f7808b7eb85969397cd
ruminose <ruminose@gmail.com>
2024-02-05 01:11:48 +0700

    Add Views

--- firstweb/myapp/views.py
+++ firstweb/myapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+#M T V {model template views}
 
 def Home(request):
     return render(request,'myapp/home.html')
@@ -9,3 +9,7 @@ def Aboutus(request):
     return render(request,'myapp/aboutus.html')
 def Contact(request):
     return render(request,'myapp/contact.html')
+def Tracking(request):
+    tracks = ['ลุง - TA#3215521TH','ลุงสมชาย - TA#321451521TH','สมศรี - TA#3212463621TH']
+    context = {'tracks':tracks}
+    return render(request,'myapp/tracking.html',context)
